67987bb597058508ff0fa4312c34fab16d37bf6d
Doron Hafner <dh9707@gmail.com>
2020-08-19 21:18:54 +0300

    ex 1-4 day4 done

---
+++ day4/jquery/exercises/main.js
@@ -0,0 +1,53 @@
+// //---------------------- exercise1,2 ----------------------
+// $('#btn1').click(function () { 
+//     let newHuman = $('#in1').val();
+//     $('#hlist').append(`<li class ='newH' >${newHuman}</li>`);
+//     $('#in1').val('');
+// });
+// $('#hlist').on('click','.newH', function () {
+//     $(this).remove()
+// });
+
+
+// //---------------------- exercise3 ----------------------
+// $('body').append('<div class="box" id="box1"> </div>');
+// $('body').append('<div class="box" id="box2"> </div>');
+// $('#box1').hover(function () {$('#box1').css('background-color', '')},
+//     function () {
+//         $('#box2').hover(function () {
+//             $('#box1').css('background-color', 'yellow')
+//         }, function () { })
+//     })
+// $('#box2').hover(function () { $('#box2').css('background-color', '')},
+//     function () {
+//         $('#box1').hover(function () {
+//             $('#box2').css('background-color', 'yellow')
+//         }, function () { })
+//     })
+
+
+
+//---------------------- exercise4 ---------------------
+// $('.item').on('click', function () {
+//     if ($(this).data().instock) {
+//         $('#cart').append(`<li>${$(this).text()}`);
+//         sum = $('li').length 
+//         $('#cart').append(`${sum} items in the cart`);
+
+//     }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+//---------------------- exercise5 ----------------------
+//---------------------- color picker ----------------------
+//---------------------- cart ----------------------

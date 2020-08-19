1cf4e2b05334df23a0f3d223f7c87475e9b62e57
Doron Hafner <dh9707@gmail.com>
2020-08-19 17:49:17 +0300

    finish all lessons

---
+++ day4/jquery/lesoons/main.js
@@ -0,0 +1,73 @@
+// // --------------------- spot-check1 ---------------------
+// const header = $('h1').css('color', 'blue')
+// console.log(header);
+
+// // --------------------- spot-check2 ---------------------
+// $('.red-div').css('color', 'red')
+// $('ul').css('color', 'green')
+// $('li:nth-child(2)').css('color', 'pink')
+// $('#brown-div').css('color', 'brown')
+
+// // --------------------- spot-check3 ---------------------
+// $('#b1, #b2').addClass('box')
+
+// // --------------------- spot-check4 ---------------------
+// $('#my-input').val('Terabyte');
+
+// // --------------------- spot-check5 ---------------------
+// console.log(`The item with the barcode ${$('#diva').data().barcode} will expire on ${$('#diva').data().expirationdate}`);
+
+// // --------------------- spot-check6 ---------------------
+// $('#back').hover( function () { $(this).css('background-color', 'blue')})
+
+// // --------------------- spot-check7 ---------------------
+// $('#sc7').on('click', function () { alert($('#my-input2').val()) })
+
+// // --------------------- spot-check8 ---------------------
+// $('.box1').hover(function () {
+//     $(this).css('background-color', 'blue')   // over
+//     }, function () {
+//         $(this).css('background-color', '')   // out
+//     }
+// );
+
+// // --------------------- spot-check9 ---------------------
+// $(".feedme").on("click", function(){
+//     let divCopy = `<div class="feedme"> ${$(this).text()} </div>`
+//     $("body").append(nd)
+//   })
+  
+// // --------------------- spot-check10 ---------------------
+// const names = [
+//     { first: "Alex", last: "Johnson" },
+//     { first: "Byron", last: "Loveall" },
+//     { first: "Cassandra", last: "Wuthers" },
+//     { first: "Deandre", last: "Rahm" },
+//     { first: "Ellena", last: "Freeman" }
+//   ]
+
+// for (const i of names) {
+//     $('body').append(`<div class='human'>${i.first}-${i.last}</div>`)
+// }
+
+// // --------------------- spot-check11 ---------------------
+// $('#remove').hover(function () {
+//     $(this).remove();
+// })
+
+// // --------------------- spot-check12 ---------------------
+// $('#sc12').on('click', function () {
+//     $('#blogs').append('<div class="blog">heres new content</div>');
+// });
+// $('#blogs').on('click', function () {
+//     $('.blog').text('blargh');
+// });
+
+// // --------------------- spot-check13 ---------------------
+// $('#sc12').on('click', function () {
+//     $('#blogs').append('<div class="blog">heres new content</div>');
+// });
+// $('#blogs').on('click',  '.blog',  function () {
+//     $(this).text('blargh');
+// });
+
